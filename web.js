f6e196fc45d69dd05d720530df897447a45149ce
yanlantao <yanlantao@hotmail.com>
2013-07-09 15:09:00 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,15 +1,23 @@
-
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var content = fs.readFile("index.html").toString();
-
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data.toString('utf-8');
+  var content = data.toString('utf-8');
 app.get('/', function(request, response) {
   response.send(content);
 });
 
+});
+
+/*
+app.get('/', function(request, response) {
+  response.send(content);
+});
+*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
