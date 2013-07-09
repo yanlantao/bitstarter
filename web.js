82ef27dbcf6049ccd20a05c2e50320d12d9faba8
yanlantao <yanlantao@hotmail.com>
2013-07-09 15:16:36 +0000

    update

--- web.js
+++ web.js
@@ -11,20 +11,9 @@ var sendResponse = function (content) {
 
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
-  console.log(data.toString('utf-8'));
   sendResponse(data.toString('utf-8'));
 });
-/*
-var sendReponse = function (content) {
-  app.get('/', function(request, response) {
-    response.send(content);
-  });
-};
 
-app.get('/', function(request, response) {
-  response.send(content);
-});
-*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
